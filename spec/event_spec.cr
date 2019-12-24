c21371960b73548b106acdb06f7a44693529abc4
elliotize <telliot16@gmail.com>
2019-12-24 13:48:11 -0500

    Fix travis (#2)
    * Fix travis
    
    * Another approach
    
    * Another approach
    
    * Another approach
    
    * start trac
    
    * Fixed the test
    
    * fixed dep

--- spec/event_spec.cr
+++ spec/event_spec.cr
@@ -3,45 +3,33 @@ include Spectrum
 describe Spectrum do
   it "should have only events that are listening to an event recieve it" do
     event_one = FooBarOneEvent.new(id: 1)
-    Spectrum.dispatch_sync_event(event_one)
-    FooBarOneEventHandler.last_event.should eq(event_one)
-    FooBarTwoEventHandler.last_event.should_not eq(event_one)
-    FooBarBothEventHandler.last_event.should eq(event_one)
     event_two = FooBarTwoEvent.new(id: 2)
-    Spectrum.dispatch_sync_event(event_two)
-    FooBarOneEventHandler.last_event.should_not eq(event_two)
-    FooBarTwoEventHandler.last_event.should eq(event_two)
-    FooBarBothEventHandler.last_event.should eq(event_two)
     event_three = FooBarOneEvent.new(id: 3)
+
+    Spectrum.dispatch_sync_event(event_one)
+    Spectrum.dispatch_sync_event(event_two)
     Spectrum.dispatch_sync_event(event_three)
-    FooBarOneEventHandler.last_event.should eq(event_three)
-    FooBarTwoEventHandler.last_event.should_not eq(event_three)
-    FooBarBothEventHandler.last_event.should eq(event_three)
+
+    event_one.handlers.size.should eq(2)
+    event_two.handlers.size.should eq(2)
+    event_three.handlers.size.should eq(2)
   end
 
   it "async events should be triggered" do
     event_queue = EventQueue.start(:test)
     event_one = FooBarOneEvent.new(id: 1)
-    Spectrum.dispatch_async_event(event_one, :test)
-    while event_queue.busy?
-    end
-    FooBarOneEventHandler.last_event.should eq(event_one)
-    FooBarTwoEventHandler.last_event.should_not eq(event_one)
-    FooBarBothEventHandler.last_event.should eq(event_one)
-    while event_queue.busy?
-    end
     event_two = FooBarTwoEvent.new(id: 2)
-    Spectrum.dispatch_async_event(event_two, :test)
-    FooBarOneEventHandler.last_event.should_not eq(event_two)
-    FooBarTwoEventHandler.last_event.should eq(event_two)
-    FooBarBothEventHandler.last_event.should eq(event_two)
-    while event_queue.busy?
-    end
     event_three = FooBarOneEvent.new(id: 3)
+
+    Spectrum.dispatch_async_event(event_one, :test)
+    Spectrum.dispatch_async_event(event_two, :test)
     Spectrum.dispatch_async_event(event_three, :test)
-    FooBarOneEventHandler.last_event.should eq(event_three)
-    FooBarTwoEventHandler.last_event.should_not eq(event_three)
-    FooBarBothEventHandler.last_event.should eq(event_three)
+    while event_queue.busy?
+      # This is just a loop to ensure there is not a race condition
+    end
+    event_one.handlers.size.should eq(2)
+    event_two.handlers.size.should eq(2)
+    event_three.handlers.size.should eq(2)
     EventQueue.stop(:test)
   end
 end
